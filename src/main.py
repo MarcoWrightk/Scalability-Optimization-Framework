0d035fe1c6d1b4fd38dad7b36c7564c20b13c432
MarcoWrightk <101480497+MarcoWrightk@users.noreply.github.com>
2024-09-30 21:08:30 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+# main.py
 
+from module1 import function1
+from module2 import function2
+
+def main():
+    # Lógica principal del programa
+    result1 = function1()
+    result2 = function2()
+    print(result1, result2)
+
+if __name__ == "__main__":
+    main()
